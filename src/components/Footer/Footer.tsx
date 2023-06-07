319bb26a40f76b17f000d15937c3b2c92ece312e
christophe.miranville <christophe.miranville@laposte.net>
2023-06-07 23:27:52 +0200

    initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import './Footer.scss';
+
+function Footer() {
+  return <div className="Footer">O + Offrant
+  Inscription
+  Connectez-vous 
+  Vendre 
+  Catégories 
+  Contactez-nous 
+  Pseudo 1 
+  Pseudo 2
+  Pseudo 3
+  Pseudo 4
+  Pseudo 5
+  </div>;
+}
+
+export default Footer;
