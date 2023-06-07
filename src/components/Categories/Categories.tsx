77bd9a9a4eeb3bf289c6cd33d0b21fcff1f10581
Estelle <estelle.lizheng@gmail.com>
2023-06-07 14:39:48 +0200

    create navbar with all categories

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,25 @@
+function Categories() {
+  return(
+    <div id="wrapper">
+      <nav className="categories-navbar">
+        <button type="button" className="categories-button">
+          Sport & Loisirs
+        </button>
+        <button type="button" className="categories-button">
+          Maison & Jardin
+        </button>
+        <button type="button" className="categories-button">
+          Ghigh Tech
+        </button>
+        <button type="button" className="categories-button">
+          Mode
+        </button>
+        <button type="button" className="categories-button">
+          Livres
+        </button>
+      </nav>
+    </div>
+  )
+}
+
+export default Categories;
