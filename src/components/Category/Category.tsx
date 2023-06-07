75bc7f676233181e03259e92df565e8648662ce0
christophe.miranville <christophe.miranville@laposte.net>
2023-06-07 13:07:29 +0200

    ajout de class pour img et button pour fix le probleme de conflit de css quand on merge

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -5,79 +5,113 @@ function Category() {
     <>
   <div className="Category">
   <p className="Tri">Trier par : 
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
-  <input className="input" type="checkbox"/>
-  <label >Catégorie</label>
+  <input className="inputCat" type="checkbox"/>
+  <label >Sport & Loisirs</label>
+  <input className="inputCat" type="checkbox"/>
+  <label >High Tech</label>
+  <input className="inputCat" type="checkbox"/>
+  <label >Maison & Jardin</label>
+  <input className="inputCat" type="checkbox"/>
+  <label >Mode</label>
+  <input className="inputCat" type="checkbox"/>
+  <label >Livres</label>
   </p>
   </div>
 
   <div className='containerCardCat'>
   <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div><div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div><div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 
 <div className='cardCat'>
-<img src="../../public/DualSense-Edge-Main.webp" alt="" />
+<img className='pictureItem' src="../../public/DualSense-Edge-Main.webp" alt="" />
 <p>Descriptif de l'objet</p>
 <p>Prix Initial : 30,00 €</p>
-<p className="liveAuction" >Prix Enchère Actuelle : 50,00 € <button>Surenchérir !</button></p>
+
+<div className="liveAuction">
+<p>Temps restant : 1:30:35</p>
+<p>Prix Enchère Actuelle : 50,00 € <button className="liveAuction-button">Surenchérir !</button></p>
+</div>
 </div>
 </div>
 
