b15aeffcc3975c10ef6acc44ca00625c6b1be2ec
zygnematophyce <lfourteau91@gmail.com>
2023-06-07 15:30:44 +0200

    mobile first: font + somes icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,11 @@
-import './Header.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faCartShopping, faToolbox, faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import "./Header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCartShopping,
+  faToolbox,
+  faCircleUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
@@ -8,23 +13,52 @@ function Header() {
       <div id="wrapper">
         <nav className="header-navbar">
           <h1 className="header-title">O+ Offrant</h1>
-          <form className="header-form">
-            <div className="searchbar-container">
-              <FontAwesomeIcon icon={faMagnifyingGlass} size="sm" style={{color: "#000000",}}/>
-              <input type="text" name= "search" className="header-searchbar" placeholder='Que cherchez-vous ?' />
+          <div className="header-container-elements">
+            <form className="header-form">
+              <div className="searchbar-container">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  size="sm"
+                  style={{ color: "#000000" }}
+                />
+                <input
+                  type="text"
+                  name="search"
+                  className="header-searchbar"
+                  placeholder="Que cherchez-vous ?"
+                />
+              </div>
+            </form>
+            <div className="wrapper-cart">
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                size="lg"
+                style={{ color: "#0c7eb4" }}
+              />
+              <button type="button" className="header-sell">
+                Vendre
+              </button>
             </div>
-          </form>
-          <div className="wrapper-cart">
-            <FontAwesomeIcon icon={faCartShopping} size="lg" style={{color: "#0c7eb4",}}/>
-            <button type='button' className="header-sell">Vendre</button>
+            <div className="wrapper-category">
+              <FontAwesomeIcon
+                icon={faToolbox}
+                size="lg"
+                style={{ color: "#0c7eb4" }}
+              />
+              <button type="button" className="header-category">
+                Catégories
+              </button>
+            </div>
+            <button type="button" className="header-login">
+              <FontAwesomeIcon
+                icon={faCircleUser}
+                size="lg"
+                style={{ color: "#ffffff" }}
+                className="icon-user"
+              />
+              Connectez-vous
+            </button>
           </div>
-          <div className="wrapper-category">
-            <FontAwesomeIcon icon={faToolbox} size="lg" style={{color: "#0c7eb4",}}/>
-            <button type='button' className="header-category">Catégories</button>
-          </div>
-          <button type='button' className="header-login">
-            <FontAwesomeIcon icon={faCircleUser} size="lg" style={{color: "#ffffff",}} className="icon-user" />
-            Connectez-vous</button>
         </nav>
       </div>
     </header>
