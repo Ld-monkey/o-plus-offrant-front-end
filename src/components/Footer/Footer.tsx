43068f2c8e8fe98297ddbd6b2fb3799188b23aa8
christophe.miranville <christophe.miranville@laposte.net>
2023-06-08 12:22:59 +0200

    mise en forme cf maquette figma

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,27 @@
 import './Footer.scss';
 
+function handleClickFooter(){
+  // Quand on clique on est redirigé vers la page correspondante
+}
+
 function Footer() {
-  return (<div className="Footer">
-  
-  <div className='Footer__info'>  O + Offrant</div> 
-  <div className='Footer__info'>Inscription</div> 
-  <div className='Footer__info'>Connectez-vous</div>
-  <div className='Footer__info'>Vendre</div>
-  <div className='Footer__info'>Catégories</div>
-  <div className='Footer__info'>Contactez-nous</div>
-  <div className='Footer__Pseudo'>Pseudo 1</div>
-  <div className='Footer__Pseudo'>Pseudo 2</div>
-  <div className='Footer__Pseudo'>Pseudo 3</div>
-  <div className='Footer__Pseudo'>Pseudo 4</div>
-  <div className='Footer__Pseudo'>Pseudo 5</div>
+  return (
+ <div className="Footer">
+  <div className="Footer__info">
+  <h2>  O + Offrant</h2> 
+  <span onClick={handleClickFooter}>Inscription</span> 
+  <span onClick={handleClickFooter}>Connectez-vous</span>
+  <span onClick={handleClickFooter}>Vendre</span>
+  <span onClick={handleClickFooter}>Catégories</span>
+  </div>
+  <div className="Footer__Pseudo">
+  <h2 onClick={handleClickFooter}>Contactez-nous</h2>
+  <span onClick={handleClickFooter}>Pseudo 1</span>
+  <span onClick={handleClickFooter}>Pseudo 2</span>
+  <span onClick={handleClickFooter}>Pseudo 3</span>
+  <span onClick={handleClickFooter}>Pseudo 4</span>
+  <span onClick={handleClickFooter}>Pseudo 5</span>
+  </div>
   </div>);
 }
 
