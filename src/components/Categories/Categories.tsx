75221fddff4ac71570d30d3e81c456d1fd064fb5
Estelle <estelle.lizheng@gmail.com>
2023-06-09 09:47:52 +0200

    fixing prettier issues: quotes and href links

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,27 +1,27 @@
-import "./Categories.scss";
+import './Categories.scss';
 
 function Categories() {
-  return(
+  return (
     <div id="wrapper">
       <nav className="categories-navbar">
-        <a href='#' className="categories-link">
+        <a href="/#" className="categories-link">
           Sport & Loisirs
         </a>
-        <a href='#' className="categories-link">
+        <a href="/#" className="categories-link">
           Maison & Jardin
         </a>
-        <a href='#' className="categories-link">
+        <a href="/#" className="categories-link">
           High Tech
         </a>
-        <a href='#' className="categories-link">
+        <a href="/#" className="categories-link">
           Mode
         </a>
-        <a href='#' className="categories-link">
+        <a href="/#" className="categories-link">
           Livres
         </a>
       </nav>
     </div>
-  )
+  );
 }
 
 export default Categories;
