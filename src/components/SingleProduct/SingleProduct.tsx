1588eb2b7aa9dd7845c6e9e9ee4a357342cb231a
Estelle <estelle.lizheng@gmail.com>
2023-06-11 18:27:44 +0200

    adding classnames to all tags of the single product page

--- src/components/SingleProduct/SingleProduct.tsx
+++ src/components/SingleProduct/SingleProduct.tsx
@@ -11,20 +11,34 @@ function SingleProduct() {
             src="https://www.slate.fr/sites/default/files/styles/1060x523/public/kbo-bike-6s20vsn5gsy-unsplash.jpg"
             alt="product"
           />
-          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} className="single-product-fave-icon" />
         </div>
-        <div>
-          <h2>Titre du produit</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eos eaque laboriosam eius pariatur blanditiis vitae sed temporibus cumque ratione ducimus repellendus ex, quaerat quae consequuntur, saepe necessitatibus? Explicabo debitis ducimus quis praesentium officia necessitatibus pariatur incidunt minima libero ex cumque, in consequatur esse cum eaque sint reiciendis ad iste?</p>
-          <span>Temps restant : 01:59:00</span>
-          <div>
-            <span>Montant actuel : 1000 Tokens</span>
-            <button type="button">Enchérir</button>
+        <div className="single-product-details">
+          <h2 className="single-product-title">Titre du produit</h2>
+          <p className="single-product-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            eos eaque laboriosam eius pariatur blanditiis vitae sed temporibus
+            cumque ratione ducimus repellendus ex, quaerat quae consequuntur,
+            saepe necessitatibus? Explicabo debitis ducimus quis praesentium
+            officia necessitatibus pariatur incidunt minima libero ex cumque, in
+            consequatur esse cum eaque sint reiciendis ad iste
+          </p>
+          <span className="auction-remaining-time">
+            Temps restant : 01:59:00
+          </span>
+          <div className="auction-amount">
+            <span className="auction-current-price">
+              Montant actuel : 1000 Tokens
+            </span>
+            <button className="auction-participate-btn" type="button">
+              Enchérir
+            </button>
           </div>
         </div>
       </section>
+
       <section className="auction-history">
-        <header>Historique des enchères</header>
+        <h2 className="auction-history-title">Historique des enchères</h2>
         {/* GRID ? */}
       </section>
     </div>
