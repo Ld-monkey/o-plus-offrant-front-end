b7d8d92597f01dbc7e5bc04f6008bb69f2fb0040
Estelle <estelle.lizheng@gmail.com>
2023-06-13 10:56:55 +0200

    Add react-router-dom dependencies and homepage route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './styles/index.scss';
 import App from './components/App/App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      path: 'produits',
+      element: 
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
