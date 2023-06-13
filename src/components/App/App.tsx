da4f4bfb04d19bd96eff619af749bac93cd9bf66
Estelle <estelle.lizheng@gmail.com>
2023-06-13 14:32:41 +0200

    feature: scroll back to the top when navigating through components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import AppHeader from '../AppHeader/AppHeader';
 import Categories from '../Categories/Categories';
 import Login from '../Login/Login';
@@ -10,10 +10,18 @@ import './App.scss';
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const location = useLocation();
 
   const checkOpenModal = () => {
     setIsOpenModal(!isOpenModal);
   };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [location]);
 
   return (
     <div className="app">
