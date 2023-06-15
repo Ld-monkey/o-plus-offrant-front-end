e17181fc18fa3e52bd291ead7f5078d8c05891e9
Estelle <estelle.lizheng@gmail.com>
2023-06-15 17:44:08 +0200

    add dynamic data WIP

--- src/components/SingleProduct/SingleProduct.tsx
+++ src/components/SingleProduct/SingleProduct.tsx
@@ -1,6 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './SingleProduct.scss';
 import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 interface SingleArticleProps {
   id: number;
@@ -13,38 +16,45 @@ interface SingleArticleProps {
 }
 
 function SingleProduct() {
+  const [article, setArticle] = useState<SingleArticleProps>();
+
+  const { idArticle } = useParams();
+
+  useEffect(() => {
+    async function fetchArticlebyId() {
+      const response = await axios.get(
+        `https://didierlam-server.eddi.cloud/api/article/${idArticle}`
+      );
+      setArticle(response.data.article);
+    }
+    fetchArticlebyId();
+  }, [idArticle]);
+
   return (
     <div id="wrapper">
       <section className="single-product">
         <div className="single-product-media">
           <img
-            src="https://www.slate.fr/sites/default/files/styles/1060x523/public/kbo-bike-6s20vsn5gsy-unsplash.jpg"
-            alt="product"
+            src={`https://didierlam-server.eddi.cloud/${article?.photo}`}
+            alt={article?.nom}
             className="photo"
           />
           <FontAwesomeIcon icon={faStar} className="fave-icon" />
         </div>
         <div className="single-product-details">
-          <h2 className="single-product-title">Titre du produit</h2>
-          <p className="single-product-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            eos eaque laboriosam eius pariatur blanditiis vitae sed temporibus
-            cumque ratione ducimus repellendus ex, quaerat quae consequuntur,
-            saepe necessitatibus? Explicabo debitis ducimus quis praesentium
-            officia necessitatibus pariatur incidunt minima libero ex cumque, in
-            consequatur esse cum eaque sint reiciendis ad iste
-          </p>
+          <h2 className="single-product-title">{article?.nom}</h2>
+          <p className="single-product-description">{article?.description}</p>
           <div className="auction-infos">
             <span className="auction-original-price">
-              Prix de départ: 500 Tokens
+              Prix de départ: {article?.prix_de_depart} Tokens
             </span>
             <span className="auction-remaining-time">
-              Temps restant : 01:59:00
+              Temps restant : {article?.date_de_fin}
             </span>
           </div>
           <div className="auction-amount">
             <span className="auction-current-price">
-              Mise actuelle : 667 Tokens
+              Mise actuelle : {article?.montant} Tokens
             </span>
             <button className="participate-btn" type="button">
               Enchérir
