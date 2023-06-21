d8d4df74747c54db8a9a545ed9bc17b6d67d0137
Estelle <estelle.lizheng@gmail.com>
2023-06-21 19:33:45 +0200

    update feature working and display an string instead of an array if empty

--- src/components/Profil/Profil.tsx
+++ src/components/Profil/Profil.tsx
@@ -6,6 +6,7 @@ import dayjs from 'dayjs';
 import './Profil.scss';
 import { useEffect, useState } from 'react';
 import axios from '../../api/axios';
+import { useAppSelector } from '../../hooks/redux';
 
 interface UserProps {
   nom: string;
@@ -34,6 +35,7 @@ interface UserWonAuctions {
 }
 
 function Profile() {
+  const [isEditing, setIsEditing] = useState(false);
   const [userInfo, setUserInfo] = useState<UserProps>({
     nom: '',
     prenom: '',
@@ -43,25 +45,58 @@ function Profile() {
   const [userAuctions, setUserAuctions] = useState<UserAuctions[]>([]);
   const [userWonAuctions, setUserWonAuctions] = useState<UserWonAuctions[]>([]);
 
-  const { idUser } = useParams();
+  const userId = useAppSelector((state) => state.user.id);
 
   useEffect(() => {
     async function fetchUserbyId() {
-      const response = await axios.get(`/api/profile/${idUser}`);
+      const response = await axios.get(`/api/profile/${userId}`);
       setUserInfo(response.data.profile);
       setUserArticles(response.data.histSell);
       setUserAuctions(response.data.histBuy);
       setUserWonAuctions(response.data.wonAuction);
     }
     fetchUserbyId();
-  }, [idUser]);
+  }, [userId]);
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  // function handleDelete() {}
+
+  function handleInputChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) {
+    setUserInfo((prevUserInfo) => ({
+      ...prevUserInfo,
+      [field]: event.target.value,
+    }));
+  }
+
+  async function handleSaveButton() {
+    try {
+      const response = await axios.patch(
+        `/api/profile/${userId}/update`,
+        userInfo
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsEditing(false);
+  }
 
   return (
     <div id="wrapper">
       <h2 className="user-title">
         Mes informations
         <div className="icons-user">
-          <FontAwesomeIcon icon={faPenToSquare} className="icon-update" />
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            className="icon-update"
+            onClick={handleEdit}
+          />
           <FontAwesomeIcon icon={faTrashCan} className="icon-delete" />
         </div>
       </h2>
@@ -73,110 +108,147 @@ function Profile() {
         </div>
 
         <div className="user-infos">
-          <span>{userInfo.nom}</span>
-          <span>{userInfo.prenom}</span>
-          <span>{userInfo.adresse_mail}</span>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={userInfo.nom}
+                onChange={(e) => handleInputChange(e, 'nom')}
+              />
+              <input
+                type="text"
+                value={userInfo.prenom}
+                onChange={(e) => handleInputChange(e, 'prenom')}
+              />
+              <input
+                type="text"
+                value={userInfo.adresse_mail}
+                onChange={(e) => handleInputChange(e, 'adresse_mail')}
+              />
+              <button type="button" onClick={handleSaveButton}>
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              <span>{userInfo.nom}</span>
+              <span>{userInfo.prenom}</span>
+              <span>{userInfo.adresse_mail}</span>
+            </>
+          )}
         </div>
       </section>
 
       <section className="my-articles">
         <h2 className="section-title">Mes articles</h2>
-        <table className="my-articles-list">
-          <thead>
-            <tr>
-              <th>Nom de l&apos;article</th>
-              <th>Mise actuelle</th>
-              <th>Date de fin de vente</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userArticles.map((userArticle) => {
-              const formattedDate = dayjs(userArticle.date_de_fin).format(
-                'DD-MM-YYYY [à] HH:mm'
-              );
-              return (
-                <tr key={userArticle.id}>
-                  <td>
-                    <Link to={`/produit/${userArticle.id}`}>
-                      {userArticle.nom}
-                    </Link>
-                  </td>
-                  <td>
-                    <span>{userArticle.montant}€</span>
-                  </td>
-                  <td>
-                    <span>{formattedDate}</span>
-                  </td>
-                  <td className="icons-column">
-                    <FontAwesomeIcon
-                      icon={faPenToSquare}
-                      className="icon-update"
-                    />
-                    <FontAwesomeIcon
-                      icon={faTrashCan}
-                      className="icon-delete"
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {userArticles.length === 0 ? (
+          <p className="empty-table">Tu n'as pas encore d'objet en vente.</p>
+        ) : (
+          <table className="my-articles-list">
+            <thead>
+              <tr>
+                <th>Nom de l&apos;article</th>
+                <th>Mise actuelle</th>
+                <th>Date de fin de vente</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userArticles.map((userArticle) => {
+                const formattedDate = dayjs(userArticle.date_de_fin).format(
+                  'DD-MM-YYYY [à] HH:mm'
+                );
+                return (
+                  <tr key={userArticle.id}>
+                    <td>
+                      <Link to={`/produit/${userArticle.id}`}>
+                        {userArticle.nom}
+                      </Link>
+                    </td>
+                    <td>
+                      <span>{userArticle.montant}€</span>
+                    </td>
+                    <td>
+                      <span>{formattedDate}</span>
+                    </td>
+                    <td className="icons-column">
+                      <FontAwesomeIcon
+                        icon={faPenToSquare}
+                        className="icon-update"
+                      />
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        className="icon-delete"
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </section>
 
       <section className="my-auctions">
         <h2 className="section-title">Mes enchères en cours</h2>
-        <table className="my-auctions-list">
-          <thead>
-            <tr>
-              <th>Nom de l&apos;article</th>
-              <th>Mise actuelle</th>
-              <th>Date de fin de vente</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userAuctions.map((userAuction) => {
-              const formattedDate = dayjs(userAuction.date).format(
-                'DD-MM-YYYY [à] HH:mm'
-              );
-              return (
-                <tr key={userAuction.id}>
-                  <td>
-                    <Link to={`/produit/${userAuction.id}`}>
-                      NOM DU PRODUIT
-                    </Link>
-                  </td>
-                  <td>{userAuction.montant}€</td>
-                  <td>{formattedDate}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {userAuctions.length === 0 ? (
+          <p className="empty-table">Pas de coup de cœur ?</p>
+        ) : (
+          <table className="my-auctions-list">
+            <thead>
+              <tr>
+                <th>Nom de l&apos;article</th>
+                <th>Mise actuelle</th>
+                <th>Date de fin de vente</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userAuctions.map((userAuction) => {
+                const formattedDate = dayjs(userAuction.date).format(
+                  'DD-MM-YYYY [à] HH:mm'
+                );
+                return (
+                  <tr key={userAuction.id}>
+                    <td>
+                      <Link to={`/produit/${userAuction.id}`}>
+                        NOM DU PRODUIT
+                      </Link>
+                    </td>
+                    <td>{userAuction.montant}€</td>
+                    <td>{formattedDate}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </section>
 
       <section className="auctions_won">
         <h2 className="section-title">Mes enchères remportées</h2>
-        <table className="auctions-won-list">
-          <thead>
-            <tr>
-              <th>Nom de l&apos;article</th>
-              <th>Mise finale</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userWonAuctions.map((userWonAuction) => (
-              <tr key={userWonAuction.id}>
-                <td>
-                  <Link to={`/produit/${userWonAuction.id}`}>
-                    {userWonAuction.nom}
-                  </Link>
-                </td>
-                <td>{userWonAuction.montant}€</td>
+        {userWonAuctions.length === 0 ? (
+          <p className="empty-table">Ça arrivera très prochainement 😉</p>
+        ) : (
+          <table className="auctions-won-list">
+            <thead>
+              <tr>
+                <th>Nom de l&apos;article</th>
+                <th>Mise finale</th>
               </tr>
-            ))}{' '}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {userWonAuctions.map((userWonAuction) => (
+                <tr key={userWonAuction.id}>
+                  <td>
+                    <Link to={`/produit/${userWonAuction.id}`}>
+                      {userWonAuction.nom}
+                    </Link>
+                  </td>
+                  <td>{userWonAuction.montant}€</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </section>
     </div>
   );
