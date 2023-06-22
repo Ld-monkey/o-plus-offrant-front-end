afc0b20c4f8463a9e120b5aeb77b5424345d5dbb
Estelle <estelle.lizheng@gmail.com>
2023-06-22 14:53:26 +0200

    merge AddArticle component to this branch

--- src/components/AddArticle/AddArticle.tsx
+++ src/components/AddArticle/AddArticle.tsx
@@ -1,71 +1,176 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './AddArticle.scss';
 
+interface CategoriesProps {
+  id: number;
+  nom: string;
+}
+
 function AddArticle() {
+  const [categories, setCategories] = useState<CategoriesProps[]>([]);
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await axios.get(
+          'https://didierlam-server.eddi.cloud/api/categories'
+        );
+        setCategories(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchCategories();
+  }, []);
+
+  // const [inputsData, setInputsData] = useState({
+  //   titre: '',
+  //   description: '',
+  //   categorie: '',
+  //   prix_de_depart: '0',
+  //   temps_de_vente: '',
+  //   photo: null,
+  // });
+
+  // function handleChange(
+  //   event: React.ChangeEvent<
+  //     HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+  //   >
+  // ) {
+  //   const newData = { ...inputsData };
+  //   newData[event.target.name] = event.target.value;
+  //   setInputsData(newData);
+  //   console.log(newData);
+  // }
+
+  // const [image, setImage] = useState(null);
+
+  // function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+  //   console.log(event.target.files);
+  //   setImage(event.target.files[0]);
+  // }
+
+  // async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   try {
+  //     const response = await axios.post(
+  //       'https://didierlam-server.eddi.cloud/article/creation/add',
+  //       {
+  //         nom: inputsData.titre,
+  //         description: inputsData.description,
+  //         categorie_id: inputsData.categorie,
+  //         prix_de_depart: inputsData.prix_de_depart,
+  //         date_de_fin: inputsData.temps_de_vente,
+  //         photo: inputsData.photo,
+  //         date_et_heure: new Date().toJSON().slice(0, 10),
+  //         utilisateur_vente_id: 3,
+  //       }
+  //     );
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  const [image, setImage] = useState<string | Blob>('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('photo', image);
+
+    const result = await axios.post(
+      'https://didierlam-server.eddi.cloud/api/images',
+      formData,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }
+    );
+  };
+
   return (
     <div id="wrapper">
-      <h2>Vendez votre article</h2>
-      <form method="post" className="add-article-form">
+      <h2>Vendre votre article</h2>
+      <form method="post" className="add-article-form" onSubmit={handleSubmit}>
         <div className="article-name">
           <label htmlFor="titre">Titre :</label>
-          <input type="text" id="titre" placeholder="Mug O'Clock" />
+          <input
+            type="text"
+            name="titre"
+            id="titre"
+            // onChange={(e) => setTitre(e.target.value)}
+            // value={inputsData.titre}
+            placeholder="Mug O'Clock"
+          />
         </div>
 
         <div className="article-description">
           <label htmlFor="description">Description :</label>
           <textarea
             rows={5}
+            name="description"
             id="description"
+            // onChange={(e) => setDescription(e.target.value)}
+            // value={inputsData.description}
             placeholder="Détail de l'article..."
           />
         </div>
 
         <div className="article-category">
           <label htmlFor="categorie">Catégorie :</label>
-          <select id="categorie" defaultValue="default-value" required>
+          <select
+            id="categorie"
+            defaultValue="default-value"
+            name="categorie"
+            // onChange={(e) => setCategorieId(e.target.value)}
+            required
+          >
             <option className="default-option" value="default-value" disabled>
               -- Veuillez-sélectionner --
             </option>
-            <option value="category-1">Catégorie 1</option>
-            <option value="category-2">Catégorie 2</option>
-            <option value="category-3">Catégorie 3</option>
-            <option value="category-4">Catégorie 4</option>
-            <option value="category-N">Catégorie N</option>
+            {categories.map((categorie) => (
+              <option key={categorie.id} value={categorie.id}>
+                {categorie.nom}
+              </option>
+            ))}
           </select>
         </div>
 
         <div className="article-price">
-          <label htmlFor="prix-de-depart">Prix de départ :</label>
-          <input type="text" id="prix-de-depart" placeholder="100 Tokens" />
+          <label htmlFor="prix-de-depart">Prix de départ (€):</label>
+          <input
+            type="number"
+            min="1"
+            name="prix_de_depart"
+            id="prix-de-depart"
+            // onChange={(e) => setPrixDeDepart(e.target.value)}
+            // value={inputsData.prix_de_depart}
+            placeholder="100"
+          />
         </div>
 
         <div className="article-timer">
           <label htmlFor="temps-de-vente">Temps de vente :</label>
-          <div className="wrapper" id="temps-de-vente">
-            <label htmlFor="short-sale">
-              <input
-                type="radio"
-                name="sale-period"
-                id="short-sale"
-                value="72 Hours"
-                defaultChecked
-              />
-              Vente sur 3 Jours
-            </label>
-            <label htmlFor="long-sale">
-              <input
-                type="radio"
-                id="long-sale"
-                name="sale-period"
-                value="168 Hours"
-              />
-              Vente sur 7 Jours
-            </label>
-          </div>
+          <input
+            type="date"
+            name="temps_de_vente"
+            id="temps-de-vente"
+            // onChange={(e) => setTempsDeVente(e.target.value)}
+            // value={inputsData.temps_de_vente}
+          />
         </div>
 
         <div className="article-photo">
           <label htmlFor="photo">Photo :</label>
-          <input type="file" accept="image/*" id="photo" />
+          <input
+            type="file"
+            accept="image/*"
+            id="photo"
+            name="photo"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
         </div>
 
         <div className="form-submit-btn">
