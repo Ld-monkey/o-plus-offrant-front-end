51a92316828c4c37a1d0230541f75854fb70df06
Ld-monkey <lfourteau91@gmail.com>
2023-06-24 12:53:29 +0200

    Add 404 page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import './ErrorPage.scss';
+
 function ErrorPage() {
   return (
     <div>
-      <img src="https://http.cat/images/404.jpg" alt="Erreur 404" />
+      <div id="wrapper">
+        <div className="ErrorPage">
+          <Link to="/">
+            <img src="https://http.cat/images/404.jpg" alt="Erreur 404" />
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
