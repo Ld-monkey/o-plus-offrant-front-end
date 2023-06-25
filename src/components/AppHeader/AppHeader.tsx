e371af25343182471357875added3ea45440be23
Ld-monkey <lfourteau91@gmail.com>
2023-06-25 19:27:46 +0200

    Create simple sidebar with transition effect

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -195,27 +195,25 @@ function AppHeader({ toggleModalLogin }: { toggleModalLogin: () => void }) {
           </nav>
         </div>
       </header>
-      {isOpen && (
-        <aside className="aside-menu">
-          <ul className="menu-items">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#food">Category</a>
-            </li>
-            <li>
-              <a href="#food-menu">Menu</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </aside>
-      )}
+      <aside className={isOpen ? 'aside-menu is-open' : 'aside-menu'}>
+        <ul className="menu-items">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#food">Category</a>
+          </li>
+          <li>
+            <a href="#food-menu">Menu</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </aside>
     </>
   );
 }
